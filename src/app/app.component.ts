7f8281eda9088601bcb4f9932b66a4c93968d982
Gabriel Palhares <pizzanigabriel@gmail.com>
2023-12-12 15:49:47 -0300

    cria componente da side bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderbarComponent } from './headerbar/headerbar.component'
+import { SidebarComponent } from './sidebar/sidebar.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderbarComponent],
+  imports: [CommonModule, RouterOutlet, HeaderbarComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
