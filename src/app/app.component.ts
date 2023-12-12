b1ea007c7a0c6bfc8c2cd0ee7d81bd7ea964f6ea
Gabriel Palhares <pizzanigabriel@gmail.com>
2023-12-12 15:58:07 -0300

    cria main content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderbarComponent } from './headerbar/headerbar.component'
+import { MainContentComponent } from './main-content/main-content.component'
 import { SidebarComponent } from './sidebar/sidebar.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderbarComponent, SidebarComponent],
+  imports: [CommonModule, RouterOutlet, HeaderbarComponent, SidebarComponent, MainContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
