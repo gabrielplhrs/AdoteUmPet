f8f2eed8a3a438bd7b22d65f795a63286da71a28
HenriqueMiossi <henriquemiossi@gmail.com>
2023-12-11 22:47:31 -0300

    Add headerbar component

---
+++ src/app/headerbar/headerbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-headerbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './headerbar.component.html',
+  styleUrl: './headerbar.component.css'
+})
+export class HeaderbarComponent {
+
+}
