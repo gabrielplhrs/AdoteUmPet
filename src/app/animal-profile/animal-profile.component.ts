144a801c4ff43fb99f5cd1d509bf71ea26205cd3
Gabriel Palhares <pizzanigabriel@gmail.com>
2023-12-13 20:42:08 -0300

    conserta tamanho search bar

---
+++ src/app/animal-profile/animal-profile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-animal-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './animal-profile.component.html',
+  styleUrl: './animal-profile.component.css'
+})
+export class AnimalProfileComponent {
+
+}
