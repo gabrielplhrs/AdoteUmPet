94eb4c7450e1335070585cbf30e5ff5fa94e5eb1
HenriqueMiossi <henriquemiossi@gmail.com>
2023-12-13 16:39:31 -0300

    Add modal-help-shelter structure

--- src/app/components/modal-help-shelter/modal-help-shelter.component.ts
+++ src/app/components/modal-help-shelter/modal-help-shelter.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ModalComponent } from '../modal/modal.component';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-modal-help-shelter',
   standalone: true,
-  imports: [ModalComponent],
+  imports: [ModalComponent, ButtonComponent],
   templateUrl: './modal-help-shelter.component.html',
   styleUrl: './modal-help-shelter.component.css'
 })
