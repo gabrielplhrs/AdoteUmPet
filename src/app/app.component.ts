1468206c1678cdacd3d7ea2e10bfb3a2334a0c6c
Gabriel Palhares <pizzanigabriel@gmail.com>
2023-12-12 21:50:51 -0300

    adiciona componente para adicionar animais

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderbarComponent } from './headerbar/headerbar.component'
 import { MainContentComponent } from './main-content/main-content.component'
 import { SidebarComponent } from './sidebar/sidebar.component'
+import { AnimalFormComponent } from './animal-form/animal-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderbarComponent, SidebarComponent, MainContentComponent],
+  imports: [CommonModule, RouterOutlet, HeaderbarComponent, SidebarComponent, MainContentComponent, AnimalFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
