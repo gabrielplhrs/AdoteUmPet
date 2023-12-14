5e25640040799b4622fe3c5b057afee333011838
Gabriel Palhares <pizzanigabriel@gmail.com>
2023-12-13 21:08:48 -0300

    finaliza tela animal-profile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,12 @@ import { SearchscreenComponent } from './screens/searchscreen/searchscreen.compo
 // import { MainContentComponent } from './components/main-content/main-content.component'
 // import { SidebarComponent } from './components/sidebar/sidebar.component'
 // import { AnimalFormComponent } from './animal-form/animal-form.component';
+import { AnimalProfileComponent } from './animal-profile/animal-profile.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderbarComponent, /* MainContentComponent, SidebarComponent, AnimalFormComponent,*/ SearchscreenComponent],
+  imports: [CommonModule, RouterOutlet, HeaderbarComponent, /* MainContentComponent, SidebarComponent, AnimalFormComponent,*/ AnimalProfileComponent, SearchscreenComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
