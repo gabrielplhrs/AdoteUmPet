8137e40b2ad7759e30fc64d9c281f62519918231
HenriqueMiossi <henriquemiossi@gmail.com>
2023-12-14 22:14:28 -0300

    Add navigation from search to login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { SearchscreenComponent } from './screens/searchscreen/searchscreen.component';
 import { AbrigoScreenComponent } from './screens/abrigo-screen/abrigo-screen.component';
 import { AnimalProfileComponent } from './screens/animal-profile/animal-profile.component';
+import { LoginScreenComponent } from './screens/login-screen/login-screen.component';
 
 export const routes: Routes = [
     { path: 'buscar', component: SearchscreenComponent },
-    // { path: 'abrigo', component: AbrigoScreenComponent },
+    { path: 'login', component: LoginScreenComponent },
     { path: 'animal', component: AnimalProfileComponent },
+    { path: 'abrigo', component: AbrigoScreenComponent },
     { path: '', redirectTo: '/buscar', pathMatch: 'full' },
 ];
--- src/app/components/headerbar/headerbar.component.ts
+++ src/app/components/headerbar/headerbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-headerbar',
@@ -8,5 +9,9 @@ import { Component } from '@angular/core';
   styleUrl: './headerbar.component.css'
 })
 export class HeaderbarComponent {
-
+  constructor(private router: Router) {}
+  
+  navigateToLogin() {
+    this.router.navigate(['/login'])
+  }
 }
