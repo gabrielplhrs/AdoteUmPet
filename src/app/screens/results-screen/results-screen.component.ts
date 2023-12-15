9396a6027c9959c746b8d5a69d4f8df94a7ab233
Gabriel Palhares <pizzanigabriel@gmail.com>
2023-12-15 00:08:47 -0300

    Implementacao do servico para bater nos endpoints da api

--- src/app/screens/results-screen/results-screen.component.ts
+++ src/app/screens/results-screen/results-screen.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { GetpetApiService } from '../../getpet-api.service';
+import { HeaderbarComponent } from '../../components/headerbar/headerbar.component';
 
 @Component({
   selector: 'app-results-screen',
   standalone: true,
-  imports: [],
+  imports: [HeaderbarComponent],
   templateUrl: './results-screen.component.html',
   styleUrl: './results-screen.component.css'
 })
-export class ResultsScreenComponent {
+export class ResultsScreenComponent implements OnInit{
 
+  animalList$!:Observable<any[]>;
+  animalList:any=[];
+
+  animalMap:Map<number, string> = new Map();
+
+  constructor(private service:GetpetApiService) {}
+
+  ngOnInit(): void {
+    this.animalList$ = this.service.getAnimalList();
+  }
 }
