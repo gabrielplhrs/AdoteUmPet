ab789872f1316a60fb8c7c2e1dcdb14ccc62b447
HenriqueMiossi <henriquemiossi@gmail.com>
2023-12-17 17:52:22 -0300

    Finish user register screen style

--- src/app/screens/register-screen/register-screen.component.ts
+++ src/app/screens/register-screen/register-screen.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { HeaderbarComponent } from '../../components/headerbar/headerbar.component';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register-screen',
   standalone: true,
-  imports: [HeaderbarComponent],
+  imports: [HeaderbarComponent, CommonModule, FormsModule],
   templateUrl: './register-screen.component.html',
   styleUrl: './register-screen.component.css'
 })
 export class RegisterScreenComponent {
+  checkboxSelected = false;
+  
+  constructor(private router: Router) {}
 
+  goToLogin() {
+    this.router.navigate(['/login']);
+  }
 }
